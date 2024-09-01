a40ebaf02d83c6e90b099326da7ccf01dfec09dc
Damien Erambert <damien@erambert.me>
2024-08-31 20:34:56 -0700

    more images

--- src/components/status.tsx
+++ src/components/status.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { getPostId, isBoost, isStatus } from "../dataHelpers";
 import type { OrderedItem } from "../types/outbox";
+import { Blurhash } from "react-blurhash";
 
 interface Props {
   status: OrderedItem;
@@ -20,8 +21,7 @@ const Status: React.FC<Props> = ({ status, isExpanded, isMain }) => {
         .filter(
           (f) => f.type === "Document" && f.mediaType.startsWith("image/")
         )
-        .filter(Boolean)
-        .slice(0, 3);
+        .filter(Boolean);
     }
     return [];
   }, [status]);
@@ -56,37 +56,61 @@ const Status: React.FC<Props> = ({ status, isExpanded, isMain }) => {
   }
 
   if (isStatus(status)) {
+    const hasSensitiveContent = status.object.sensitive || true;
+
     const statusContent = (
       <>
         <div
           className="status-body"
           dangerouslySetInnerHTML={{ __html: status.object.content }}
         />
+
         {(images.length && (
-          <div
-            className="status-images"
-            style={{
-              aspectRatio:
-                images.length === 1
-                  ? `${images[0].width} / ${images[0].height}`
-                  : undefined,
-            }}
-          >
-            {images.map((f, index) => {
-              console.log(f);
-              return (
-                <a key={index} href={f.url}>
-                  <img
-                    src={f.url}
-                    height={f.height}
-                    width={f.width}
-                    alt={f.summary || "attachment"}
-                    className="status-image"
-                    loading="lazy"
-                  />
-                </a>
-              );
-            })}
+          <div className="status-media">
+            {hasSensitiveContent && (
+              <label
+                className="status-image-toggle"
+                htmlFor={`${status.id}-media-toggle`}
+              >
+                {eyeSlashIcon}
+                <input type="checkbox" id={`${status.id}-media-toggle`} />
+                <span>Sensitive content, click to show</span>
+              </label>
+            )}
+            <div
+              className="status-images"
+              style={{
+                aspectRatio:
+                  images.length === 1
+                    ? `${images[0].width} / ${images[0].height}`
+                    : undefined,
+              }}
+            >
+              {images.map((f, index) => {
+                return (
+                  <a key={index} href={f.url}>
+                    {f.blurhash && hasSensitiveContent && (
+                      <Blurhash
+                        hash={f.blurhash}
+                        height={f.height}
+                        width={f.width}
+                        resolutionX={32}
+                        resolutionY={32}
+                        punch={1}
+                      ></Blurhash>
+                    )}
+                    <img
+                      src={f.url}
+                      height={f.height}
+                      width={f.width}
+                      alt={f.summary || "attachment"}
+                      className="status-image"
+                      loading="lazy"
+                    />
+                  </a>
+                );
+              })}
+            </div>
           </div>
         )) ||
           null}
@@ -117,3 +141,15 @@ const Status: React.FC<Props> = ({ status, isExpanded, isMain }) => {
 };
 
 export default Status;
+
+const eyeSlashIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    height="24"
+    viewBox="0 -960 960 960"
+    width="24"
+    aria-hidden="true"
+  >
+    <path d="m644-428-58-58q9-47-27-88t-93-32l-58-58q17-8 34.5-12t37.5-4q75 0 127.5 52.5T660-500q0 20-4 37.5T644-428Zm128 126-58-56q38-29 67.5-63.5T832-500q-50-101-143.5-160.5T480-720q-29 0-57 4t-55 12l-62-62q41-17 84-25.5t90-8.5q151 0 269 83.5T920-500q-23 59-60.5 109.5T772-302Zm20 246L624-222q-35 11-70.5 16.5T480-200q-151 0-269-83.5T40-500q21-53 53-98.5t73-81.5L56-792l56-56 736 736-56 56ZM222-624q-29 26-53 57t-41 67q50 101 143.5 160.5T480-280q20 0 39-2.5t39-5.5l-36-38q-11 3-21 4.5t-21 1.5q-75 0-127.5-52.5T300-500q0-11 1.5-21t4.5-21l-84-82Zm319 93Zm-151 75Z"></path>
+  </svg>
+);
